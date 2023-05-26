ecc53f79c9904063145d55aabc6e236e7f2b75b7
RomanNestorko <104514561+RomanXz92@users.noreply.github.com>
2023-05-26 22:05:00 +0300

    play off

---
+++ homework-04/middleWares/index.js
@@ -0,0 +1,6 @@
+const isValidId = require('./isValid');
+const authentificate = require('./authentificate');
+module.exports = {
+  isValidId,
+  authentificate,
+};
---
+++ homework-04/modals/user.js
@@ -0,0 +1,55 @@
+const { Schema, model } = require('mongoose');
+const { handleMongooseError } = require('../utils');
+const Joi = require('joi');
+
+const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+const userSchema = new Schema(
+  {
+    password: {
+      type: String,
+      required: [true, 'Set password for user'],
+    },
+    email: {
+      type: String,
+      match: emailRegex,
+      required: [true, 'Email is required'],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ['starter', 'pro', 'business'],
+      default: 'starter',
+    },
+    token: {
+      type: String,
+      default: '',
+    },
+  },
+  { versionKey: false, timestamps: true },
+);
+
+userSchema.post('save', handleMongooseError);
+
+const userRegistrSchema = Joi.object({
+  password: Joi.string().required(),
+  email: Joi.string().pattern(emailRegex).required(),
+  subscription: Joi.string().required(),
+});
+
+const userLoginSchema = Joi.object({
+  password: Joi.string().required(),
+  email: Joi.string().pattern(emailRegex).required(),
+});
+
+const schemas = {
+  userRegistrSchema,
+  userLoginSchema,
+};
+
+const User = model('user', userSchema);
+
+module.exports = {
+  User,
+  schemas,
+};
